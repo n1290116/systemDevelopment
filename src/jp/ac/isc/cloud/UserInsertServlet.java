3987e7a1764b0991dbc62c4b09b9768aa49fe31e
n1290116 <bjha1290116@gn.iwasaki.ac.jp>
2020-09-15 10:11:05 +0900

    練習02_1

---
+++ src/jp/ac/isc/cloud/UserInsertServlet.java
@@ -0,0 +1,45 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class UserInsertServlet
+ */
+@WebServlet("/UserInsertServlet")
+public class UserInsertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			Connection users = null;
+			try {
+			//request.setCharacterEncoding("utf-8");
+			users = DBConnection.openConnection();
+			String id = request.getParameter("insertId");
+			String name = request.getParameter("insertName");
+			String picture = request.getParameter("insertPicture");
+			Statement state = users.createStatement();
+			state.executeUpdate("INSERT INTO user_table VALUE('" + id + "','" + name +"','" + picture + "')");
+			DBConnection.closeConnection(users, state);
+			response.sendRedirect("/select"); //UserSelectServletを呼び出す
+			}catch(SQLException e){
+			e.printStackTrace();
+			}
+	}
+
+}
